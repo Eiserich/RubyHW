1261cf6a2ed1071ad12908f2da6d1d349461468c
Eiserich <koroviakovskiy@ukr.net>
2022-11-13 20:21:21 +0200

    HW4 done

--- HW4/lib/app.rb
+++ HW4/lib/app.rb
@@ -1,6 +1,5 @@
 require "erb"
 require_relative "pet"
-
 class App
   attr_reader :request, :path
   attr_accessor :pet
@@ -14,89 +13,89 @@ class App
   def response
     case @request.path
 
-    when "/" then rack_response("pet_name.html.erb"))
+    when "/" then rack_response("pet_name.html.erb")
 
     when "/create_pet" then name = request.params["name"]
     return response_redirect("/") if name.nil? || name.length.zero?
     @pet = Pet.new(name)
-      response_redirect("/game")
+    response_redirect("/game")
 
     when "/game"
       return response_redirect("/") if pet.nil?
-        rack_response("index.html.erb"))
+      rack_response("index.html.erb")
 
     when "/feed"
       return response_redirect("/") if pet.nil?
       if pet.alive
-         pet.feed
-         rack_response("index.html.erb"))
+        pet.feed
+        rack_response("index.html.erb")
       else
-         response_redirect("/game_over")
+        response_redirect("/game_over")
       end
 
     when "/play"
       return response_redirect("/") if pet.nil?
       if pet.alive
-         pet.play
-         rack_response("index.html.erb"))
-      else
-         response_redirect("/game_over")
+        pet.play
+        rack_response("index.html.erb")
+        response_redirect("/game_over")
       end
 
     when "/clean"
       return response_redirect("/") if pet.nil?
       if pet.alive
-         pet.clean
-         rack_response("index.html.erb"))
+        pet.clean
+        rack_response("index.html.erb")
       else
-         response_redirect("/game_over")
+        response_redirect("/game_over")
       end
 
     when "/walk"
       return response_redirect("/") if pet.nil?
       if pet.alive
-         pet.walk
-         rack_response("index.html.erb"))
+        pet.walk
+        rack_response("index.html.erb")
       else
-         response_redirect("/game_over")
+        response_redirect("/game_over")
       end
 
     when "/vet"
       return response_redirect("/") if pet.nil?
       if pet.alive
-         pet.vet
-         rack_response("index.html.erb"))
+        pet.vet
+        rack_response("index.html.erb")
       else
-         response_redirect("/game_over")
+        response_redirect("/game_over")
       end
 
     when "/sleep"
       return response_redirect("/") if pet.nil?
       if pet.alive
-         pet.sleep
-         rack_response("index.html.erb"))
+        pet.sleep
+        rack_response("index.html.erb")
       else
          response_redirect("/game_over")
       end
 
     when "/game_over"
       @pet = nil
-      rack_response("the_end.html.erb"))
+      rack_response("the_end.html.erb")
     else response_redirect("/")
-  end
+    end
   end
 
-    def render(template)
+  def render(template)
     path = File.expand_path("../views/#{template}", __FILE__)
     ERB.new(File.read(path)).result(binding)
-    end
+  end
 
   def rack_response(template)
     Rack::Response.new(render(template))
   end
+
   def response_redirect(path)
     Rack::Response.new do |response|
       response.redirect(path)
     end
-    end
   end
+end
